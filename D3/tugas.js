551f5672ee87ebdb86952a9e45437faa2fc52ae4
Syahril Fitrawan Abadi <syahrilfitrawanabadi@gmail.com>
2024-09-20 17:24:09 +0800

    Add: Latihan day 3 done

---
+++ D3/tugas.js
@@ -0,0 +1,48 @@
+//* Latihan Day 3
+
+const mobils = [
+  {
+    "no plat": "B 1234 CD",
+    nama: "Avanza",
+    brand: "Toyota",
+    warna: ["putih", "silver"],
+    "bahan bakar": ["bensin", "gas"],
+    "maksimal penumpang": 7,
+  },
+  {
+    "no plat": "D 5678 EF",
+    nama: "Xpander",
+    brand: "Mitsubishi",
+    warna: ["hitam", "merah"],
+    "bahan bakar": ["bensin"],
+    "maksimal penumpang": 7,
+  },
+  {
+    "no plat": "F 9012 GH",
+    nama: "Jazz",
+    brand: "Honda",
+    warna: ["biru", "putih"],
+    "bahan bakar": ["bensin"],
+    "maksimal penumpang": 5,
+  },
+  {
+    "no plat": "L 3456 IJ",
+    nama: "Innova",
+    brand: "Toyota",
+    warna: ["abu-abu", "hitam"],
+    "bahan bakar": ["bensin", "solar"],
+    "maksimal penumpang": 8,
+  },
+  {
+    "no plat": "N 7890 KL",
+    nama: "Ertiga",
+    brand: "Suzuki",
+    warna: ["merah", "silver"],
+    "bahan bakar": ["bensin", "gas"],
+    "maksimal penumpang": 7,
+  },
+];
+
+for (let mobil of mobils) {
+  console.log(mobil);
+}
